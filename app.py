4c5d176b6a598389723d52907637dcf159c6a7d5
Leonardo Oliveira <ti.userleo@hotmail.com>
2022-01-04 10:46:10 -0300

    Opening a site in browser using the driver

--- app.py
+++ app.py
@@ -1,2 +1,13 @@
+from selenium import webdriver
+from email_data import EMAIL_LOGIN, EMAIL_PASSWORD
+
 class ChromeAuto:
-    pass
+    def __init__(self):
+        self._email_login = EMAIL_LOGIN
+        self._email_password = EMAIL_PASSWORD
+        self.driver = webdriver.Chrome('./chromedriver/chromedriver')
+
+    def open_browser(self):
+        self.driver.get('https://www.google.com.br/')
+
+        
--- main.py
+++ main.py
@@ -1,2 +1,5 @@
-from email import USER_EMAIL, USER_PASSWORD
 from app import ChromeAuto
+
+chrome_auto = ChromeAuto()
+
+chrome_auto.open_browser()
